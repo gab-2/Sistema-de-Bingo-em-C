bdc28835634333546f1dac0419587b56a8d0d700
Kayky Martins Pereira <blacktauz30@outlook.com>
2022-05-24 00:01:41 -0300

    Menu #03

--- Menu/Menu.c
+++ Menu/Menu.c
@@ -2,12 +2,21 @@
 #include <stdlib.h>
 #include <math.h>
 
+void Pessoa()
+{
 
+    struct DadoParticipante
+    {
 
+        char nome[30];
+        int numero;
+        int DocumetoPessoal;
+    };
 
+}
 
-void MenuInicial (){
+void MenuInicial()
+{
 
-printf("Este e para o bingo ");
-
+    printf("Este e para o bingo ");
 }
