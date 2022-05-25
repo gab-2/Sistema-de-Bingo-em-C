9748df8a151912876fba3a806809edd16dcc26ab
Kayky Martins Pereira <blacktauz30@outlook.com>
2022-05-25 01:13:50 -0300

    Menu #11

--- Menu/Menu.c
+++ Menu/Menu.c
@@ -15,27 +15,42 @@ void Pessoa()
 
     struct DadosParticipante Dados;
 
-    printf("\n Cadatrar novo Participante \n");
-    printf("\n Digite o nome do Participante: \n");
-    scanf("%c", &Dados.nome);
-    printf("\n Digite o numero do Participante: \n");
+    printf("\n Cadatrar novo Participante!");
+    printf("\n Digite o nome do Participante: ");
+    scanf("%s", &Dados.nome);
+    printf("\n Digite o numero do Participante: ");
     scanf("%d", &Dados.numero);
-    printf("\n Digite o numero do Documento Pessoal do Perticipante: \n");
+    printf("\n Digite o numero do Documento Pessoal do Perticipante: ");
     scanf("%d", &Dados.DocumetoPessoal);
 }
 
 void Premios()
-{   
+{
     struct CadastroDePremios
     {
-    char nomeDoPremio[30];
-    int ValorDoPremio;
+        char nomeDoPremio[30];
+        int ValorDoPremio;
     };
     struct CadastroDePremios Premios;
-    printf("\n Informe o premio para cadastro \n");
-    scanf("%c", &Premios.nomeDoPremio);
-    printf("\n Informe o valor do premio \n");
+    printf("\n Informe o premio para cadastro ");
+    scanf("%s", &Premios.nomeDoPremio);
+    printf("\n Informe o valor do premio ");
     scanf("%d", &Premios.ValorDoPremio);
+}
+
+void IntervaloNumeros()
+{
+
+    int intervalo_numeros;
+
+    printf("\n Digite o intervalo de numeros a serem sorteados!");
+    printf("\n EX: (1 a 50)");
+    printf("Intervalo: ");
+    scanf("%d", intervalo_numeros);
+}
+
+void NumerosPorParticipante()
+{
 }
 
 void RegistrarCartela()
@@ -47,14 +62,15 @@ void main()
 
     int opcao;
 
-    printf("---------------Menu--------------\n");
-    printf("1 - Cadastrar participante\n");
-    printf("2 - Cadastrar Premios\n");
-    printf("3 - Selecionar o intervalo dos numeros\n");
-    printf("4 - Quantidade de numeros por participante\n");
-    printf("5 - Registrar cartela\n");
-    printf("6 - Iniciar o Bingo!\n");
-    printf("Selecione a opcao desejada: ");
+    printf("\n ---------------Menu--------------");
+    printf("\n 1 - Cadastrar participante");
+    printf("\n 2 - Cadastrar Premios");
+    printf("\n 3 - Selecionar o intervalo dos numeros");
+    printf("\n 4 - Quantidade de numeros por participante");
+    printf("\n 5 - Registrar cartela");
+    printf("\n 6 - Iniciar o Bingo!");
+    printf("\n ---------------------------------");
+    printf("\n Selecione a opcao desejada: ");
     scanf("%d", &opcao);
 
     while (opcao != 6)
@@ -64,12 +80,30 @@ void main()
         {
 
             Pessoa();
-
         }
 
-        if (opcao == 2)
+        else if (opcao == 2)
         {
             Premios();
+        }
+
+        else if (opcao == 3)
+        {
+        }
+
+        else if (opcao == 4)
+        {
+        }
+
+        else if (opcao == 5)
+        {
+        }
+
+        while (opcao < 1 || opcao > 6)
+        {
+            printf("Opcao digitada invalida tente novamente!\n");
+            printf("Selecione a opcao desejada: ");
+            scanf("%d", &opcao);
         }
     }
 }
