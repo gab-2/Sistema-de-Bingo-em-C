f004801cf2566534f4feac53b69bc19097afcb5c
Tiago Oliveira <92068949+Titopollice@users.noreply.github.com>
2022-05-29 01:46:30 -0300

    Co-authored-by: Kayky Martins Pereira <Blackburn060@users.noreply.github.com>

--- Menu/Menu.c
+++ Menu/Menu.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
+#include <string.h>
 #include <math.h>
 
 // Função e struc para coletar os dados do participante
@@ -15,13 +17,22 @@ void Pessoa()
 
     struct DadosParticipante Dados;
 
+    int Qtd_participantes;
+
+    printf("\n Digite a quantidade de participantes que serao cadastrados: ");
+    scanf("%d", &Qtd_participantes);
+
     printf("\n Cadatrar novo Participante!");
-    printf("\n Digite o nome do Participante: ");
-    scanf("%s", &Dados.nome);
-    printf("\n Digite o numero do Participante: ");
-    scanf("%d", &Dados.numero);
-    printf("\n Digite o numero do Documento Pessoal do Perticipante: ");
-    scanf("%d", &Dados.DocumetoPessoal);
+
+    for (int i = 0; i < Qtd_participantes; i++)
+    {
+        printf("\n Digite o nome do Participante: ");
+        scanf("%s", &Dados.nome);
+        printf("\n Digite o numero do Participante: ");
+        scanf("%d", &Dados.numero);
+        printf("\n Digite o numero do Documento Pessoal do Perticipante: ");
+        scanf("%d", &Dados.DocumetoPessoal);
+    }
 }
 
 void Premios()
@@ -31,7 +42,17 @@ void Premios()
         char nomeDoPremio[30];
         int ValorDoPremio;
     };
+
     struct CadastroDePremios Premios;
+
+    int Qtd_participantes, Qtd_premios;
+
+    while (Qtd_premios > Qtd_participantes)
+    {
+
+        printf("A quantidade de premios nao pode ser maior que a quantidade de participantes!");
+    }
+
     printf("\n Informe o premio para cadastro ");
     scanf("%s", &Premios.nomeDoPremio);
     printf("\n Informe o valor do premio ");
@@ -62,19 +83,19 @@ void main()
 
     int opcao;
 
-    printf("\n ---------------Menu--------------");
-    printf("\n 1 - Cadastrar participante");
-    printf("\n 2 - Cadastrar Premios");
-    printf("\n 3 - Selecionar o intervalo dos numeros");
-    printf("\n 4 - Quantidade de numeros por participante");
-    printf("\n 5 - Registrar cartela");
-    printf("\n 6 - Iniciar o Bingo!");
-    printf("\n ---------------------------------");
-    printf("\n Selecione a opcao desejada: ");
-    scanf("%d", &opcao);
-
-    while (opcao != 6)
+    do
     {
+
+        printf("\n ---------------Menu--------------");
+        printf("\n 1 - Cadastrar participante");
+        printf("\n 2 - Cadastrar Premios");
+        printf("\n 3 - Selecionar o intervalo dos numeros");
+        printf("\n 4 - Quantidade de numeros por participante");
+        printf("\n 5 - Registrar cartela");
+        printf("\n 6 - Iniciar o Bingo!");
+        printf("\n ---------------------------------");
+        printf("\n Selecione a opcao desejada: ");
+        scanf("%d", &opcao);
 
         if (opcao == 1)
         {
@@ -105,5 +126,5 @@ void main()
             printf("Selecione a opcao desejada: ");
             scanf("%d", &opcao);
         }
-    }
+    } while (opcao != 6);
 }
