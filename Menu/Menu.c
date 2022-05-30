b16a39364d3f6bf5eba2a19d36d8857341266382
Kayky Martins Pereira <blacktauz30@outlook.com>
2022-05-30 01:30:15 -0300

    Armazenando dados em um arquivo! #1

--- Menu/Menu.c
+++ Menu/Menu.c
@@ -22,16 +22,16 @@ void Pessoa()
     printf("\n Digite a quantidade de participantes que serao cadastrados: ");
     scanf("%d", &Qtd_participantes);
 
+    FILE *cadastro_participante;
+    char participante[Qtd_participantes];
+
     printf("\n Cadatrar novo Participante!");
 
     for (int i = 0; i < Qtd_participantes; i++)
     {
 
-        FILE *cadastro_participante;
-        char participante[Qtd_participantes];
-
         // Função que cria uma nova string usando uma variavel, usada para o nome do arquivo que vai ser criado
-        sprintf(participante, "participante-%s.txt", i);
+        sprintf(participante, "participante-%d.txt", i);
         cadastro_participante = fopen(participante, "w");
 
         if (cadastro_participante != NULL)
@@ -39,15 +39,15 @@ void Pessoa()
 
             printf("\n Digite o nome do Participante: ");
             scanf("%s", &Dados.nome);
-            fprintf(cadastro_participante,"%s\n", Dados.nome);
+            fprintf(cadastro_participante, "%s\n", Dados.nome);
 
             printf("\n Digite o numero do Participante: ");
             scanf("%d", &Dados.numero);
-            fprintf(cadastro_participante,"%d\n", Dados.numero);
+            fprintf(cadastro_participante, "%d\n", Dados.numero);
 
             printf("\n Digite o numero do Documento Pessoal do Perticipante: ");
             scanf("%d", &Dados.DocumetoPessoal);
-            fprintf(cadastro_participante,"%d\n", Dados.DocumetoPessoal);
+            fprintf(cadastro_participante, "%d\n", Dados.DocumetoPessoal);
 
             fclose(cadastro_participante);
         }
