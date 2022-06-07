6cb6c7ff12a02fab67738f714026119a71c007bd
MarceloTizo <106279700+MarceloTizo@users.noreply.github.com>
2022-06-07 17:39:27 -0300

    menu

--- Menu/Menu.c
+++ Menu/Menu.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 #include <string.h>
 #include <math.h>
 #include <time.h>
@@ -9,34 +10,45 @@
 #include "RegistrarCartela.c"
 #include "IniciarBingo.c"
 #include <ctype.h>
-#include <windows.h>
 
 void main()
 {
-
-    UINT CPAGE_UTF8 = 65001;
-    UINT CPAGE_DEFAULT = GetConsoleOutputCP();
-    SetConsoleOutputCP(CPAGE_UTF8);
 
     int opcao;
 
     do
     {
 
+     /*
+          ┌──────────────────────────────────────────────┐
+          │ ╔════════════════════════════════════════════╧═╗
+          │ ║                                              ║
+          │ ║    ██████╗ ██╗███╗   ██╗ ██████╗  ██████╗    ║
+          │ ║    ██╔══██╗██║████╗  ██║██╔════╝ ██╔═══██╗   ║
+          │ ║    ██████╔╝██║██╔██╗ ██║██║  ███╗██║   ██║   ║
+          │ ║    ██╔══██╗██║██║╚██╗██║██║   ██║██║   ██║   ║
+          │ ║    ██████╔╝██║██║ ╚████║╚██████╔╝╚██████╔╝   ║
+          │ ║    ╚═════╝ ╚═╝╚═╝  ╚═══╝ ╚═════╝  ╚═════╝    ║
+          │ ║                                              ║
+          └─║     --- BOA SORTE NO BINGO ---               ║
+            ╚══════════════════════════════════════════════╝
+    */
+
+
         printf("\n ---------------Menu-------------------------------------------------");
         printf("\n 1 - Cadastrar participante");
-        printf("\n 2 - Cadastrar Prêmios");
-        printf("\n 3 - Quantidade de números por participante e o intervalo do sorteio");
+        printf("\n 2 - Cadastrar Premios");
+        printf("\n 3 - Quantidade de numeros por participante e o intervalo do sorteio");
         printf("\n 4 - Registrar cartela");
         printf("\n 5 - Iniciar o Bingo!");
         printf("\n --------------------------------------------------------------------");
-        printf("\n Selecione a opção desejada: ");
+        printf("\n Selecione a opcao desejada: ");
         scanf("%d", &opcao);
 
         while (opcao < 1 || opcao > 5)
         {
-            printf("\n Opção digitada inválida, tente novamente!");
-            printf("\n Selecione a opção desejada: ");
+            printf("\n Opcao digitada invalida tente novamente!");
+            printf("\n Selecione a opcao desejada: ");
             scanf("%d", &opcao);
         }
 
